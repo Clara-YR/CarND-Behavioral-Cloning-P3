8152133ceacb03b6723fe1b582e33269d5849991
Clara-YR <30821609+Clara-YR@users.noreply.github.com>
2018-01-23 07:32:00 +0800

    1st submission, train by model.py

--- model.py
+++ model.py
@@ -69,13 +69,13 @@ model = Sequential()
 model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(160, 320, 3)))
 # 4. CROPPING IMAGES
 model.add(Cropping2D(cropping=((70, 25),(0,0))))
-model.add(Convolution2D(24, (5, 5), activation="relu", strides=(2, 2)))
+model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation="relu"))
 model.add(Dropout(0.7))
-model.add(Convolution2D(36, (5, 5), activation="relu", strides=(2, 2)))
+model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation="relu"))
 model.add(Dropout(0.7))
-model.add(Convolution2D(48, (5, 5), activation="relu", strides=(2, 2)))
-model.add(Convolution2D(64, (3, 3), activation="relu"))
-model.add(Convolution2D(64, (3, 3), activation="relu"))
+model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation="relu"))
+model.add(Convolution2D(64, 3, 3, activation="relu"))
+model.add(Convolution2D(64, 3, 3, activation="relu"))
 model.add(Flatten())
 model.add(Dense(100))
 model.add(Dense(50))
@@ -83,12 +83,11 @@ model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
-model.fit_generator(train_generator,
+model.fit_generator(train_generator, 
+					samples_per_epoch=len(train_samples)*6, 
 					validation_data=validation_generator,
-					verbose=1,
-                    steps_per_epoch=len(train_samples)*6,
-                    epochs=5,
-                    validation_steps=len(validation_samples)*6)
+					nb_val_samples=len(validation_samples)*6, 
+					nb_epoch=5, verbose=1)
 
 model.save('model.h5')
 print('The model has been saved successfully.')
